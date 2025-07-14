e6ac6b5e241bd471d16ec7562c3b36eade7976be
Ismailsinan2003 <143427342+Ismailsinan2003@users.noreply.github.com>
2025-07-14 17:15:56 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,20 @@
+let score="33ab"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let value=Number(score)
+console.log(typeof value);
+console.log(val);
+
+
+let loggedIn="sinan"
+console.log(typeof(loggedIn));
+
+let BooleanLoggedIn=Boolean(loggedIn)
+console.log(typeof BooleanLoggedIn);
+console.log(BooleanLoggedIn);
+
+
+
+
