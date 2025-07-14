4a9e0dd867d1e25342f85414576cd560b89aef3e
Ismailsinan2003 <143427342+Ismailsinan2003@users.noreply.github.com>
2025-07-14 16:33:10 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,23 @@
+
+// primitive
+
+// number => numerical values both integer and floating point
+// BigInt => use BigInt when your number value exceeds 2 power 53
+// String => textual data
+// Boolean => true or false
+// undefined => a variable declared but not assigned any value
+// null => intentionally abscent of any object value
+// symbol => mostly used in react
+
+let a=22
+let profit=34569927937656787252765n
+let uname="sinan"
+let entered=true
+let age;
+let blackmark=null
+
+console.table([typeof(a),typeof(profit),typeof(uname),typeof(entered),typeof(age),typeof(blackmark)])
+
+
+
+
