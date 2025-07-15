92ae63c46dbc1ebf4a7c6ea40d2e2a40b7ba696c
Ismailsinan2003 <143427342+Ismailsinan2003@users.noreply.github.com>
2025-07-15 22:28:54 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,14 @@
+// *************************************Operations**********************************************************//
+
+
+let a = 5;
+console.log(a++);  // Output: 5 ✅
+console.log(a);    // Output: 6 ✅
+console.log(++a);  // Output: 7 ✅
+console.log(a);    // Output: 7 ✅
+
+
+let x = 3;
+let y = x++ + ++x;
+console.log(x);
+console.log(y);
