4853960948776af6198aebca632e683d18fcf611
Ismailsinan2003 <143427342+Ismailsinan2003@users.noreply.github.com>
2025-07-16 08:40:45 +0000

    return types

---
+++ 01_basics/returntype.js
@@ -0,0 +1,17 @@
+///  Retuen type of variables in javascript/////
+
+//1) Primitive
+  
+    //   number=> number
+    //   string=>string
+    //   boolean => boolean
+    //   null => object
+    //   undefined => undefined
+    //   symbol=>symbol
+    //   bigInt=>bigInt
+
+//2) NON-Primitive
+
+    // arrays=>Object
+    // function=>function
+    // Object=>Object
