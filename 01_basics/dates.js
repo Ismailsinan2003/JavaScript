7b13d966178cb9f46b019e8f9075ff8e38b4344b
Ismailsinan2003 <143427342+Ismailsinan2003@users.noreply.github.com>
2025-07-21 15:19:47 +0000

    dates

---
+++ 01_basics/dates.js
@@ -0,0 +1,34 @@
+// let myDate= new Date()
+// console.log(myDate) //2025-07-21T14:38:28.214Z
+// console.log(myDate.toDateString()) // Mon Jul 21 2025
+// console.log(myDate.toISOString())  // 2025-07-21T14:38:28.214Z
+// console.log(myDate.toJSON())       // 2025-07-21T14:38:28.214Z
+// console.log(myDate.toLocaleDateString()) // 7/21/2025
+// console.log(myDate.toLocaleString())     // 7/21/2025, 2:38:28 PM
+// console.log(myDate.toLocaleTimeString()) // 2:38:28 PM
+// console.log(myDate.toTimeString())       // 14:38:28 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toUTCString())        // Mon, 21 Jul 2025 14:38:28 GMT
+
+
+// let myBirthDate= new Date(2003, 7, 30, 5, 6, 33)
+// let myBirthDate2= new Date("2003-08-09")
+// console.log(myBirthDate.toDateString())
+// console.log(myBirthDate.toLocaleString())
+
+
+// let myTimeStamp=Date.now()
+// console.log(myTimeStamp)
+// console.log(myBirthDate2.getTime())
+
+// let dates=new Date()
+// console.log("today is :",dates.toLocaleDateString())
+// console.log("current time is :",dates.toLocaleTimeString())
+
+// let birthday=new Date("2003-08-09")
+// let d= new Date()
+
+// console.log(birthday.getFullYear())
+// console.log(d.getFullYear())    
+
+// let age=d.getFullYear()-birthday.getFullYear()
+// console.log(age)
