f61a9da4a4e0996c499d052ce0eceb8b91e31512
Ismailsinan2003 <143427342+Ismailsinan2003@users.noreply.github.com>
2025-07-23 13:06:42 +0000

    objects

---
+++ 02_basics/object2.js
@@ -0,0 +1,36 @@
+let tinderUser={}
+
+tinderUser.id=34
+
+tinderUser.mail="sinan@google.com"
+tinderUser.name={
+    fullname:{
+        firstname:"ismail",
+        lastname:"sinan"
+    }
+}
+// console.log(tinderUser)
+
+// console.log(tinderUser.name.fullname.lastname)
+
+const o1={0:"b",1:"c"}
+const o2={0:"a",1:"c"}
+
+// const o3=Object.assign({},o1, o2)
+
+const o3={...o1,...o2}
+// console.log(o3)
+
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+
+let course={
+    course_name:"javaScript",
+    course_price:"1299",
+    course_instructor:"Santosh"
+}
+
+// console.log(course.course_instructor)
+
+const {course_instructor:instructor} =course
+console.log(instructor)
