0efd2cb94ccbc8d6c48aae5544ef06106def4abe
Ismailsinan2003 <143427342+Ismailsinan2003@users.noreply.github.com>
2025-07-23 13:51:32 +0000

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,14 @@
+// function addtwonumbers(n1,n2){
+//     return (n1+n2)
+// }
+
+// result=(addtwonumbers(3,4))
+// console.log(result)
+
+
+function greet(name ="visitor",place){
+    let greet=`hello ${name}, welcome to ${place}`
+    return greet
+}
+
+console.log(greet(undefined,"Dubai"))
