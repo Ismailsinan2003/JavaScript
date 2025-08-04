72271221dbaa68f4cc76ccfc5bf0e00659be1322
Ismail Sinan <143427342+Ismailsinan2003@users.noreply.github.com>
2025-08-04 14:23:28 +0000

    promises

---
+++ 10_advance/promises.js
@@ -0,0 +1,82 @@
+// const promiseOne = new Promise(function (resolve, reject) {
+//     setTimeout(function (){
+//         console.log("task completed")
+//         resolve()
+//     },1000)
+// })
+
+// promiseOne.then(function() {
+//     console.log("promise consumed")
+// })
+
+
+// new Promise( function(resolve, reject) {
+//     setTimeout(function () {
+//         console.log("task 2 completed")
+//         resolve()
+//     },1000)
+// }).then(function () {
+//     console.log("task resolved")
+// })
+
+// const promiseThree= new Promise( function (resolve, reject) {
+//     setTimeout(function (){
+//         resolve({username:"sinan", email:"sinan@example.com"})
+//     },1000)
+// })
+
+// promiseThree.then(function (info){
+//         console.log(info)
+// })
+
+
+// const promiseFour=new Promise(function (resolve, reject){
+//     setTimeout(function () {
+//         let error = true
+//         if(!error){
+//             resolve({username:"sinan", age:22})
+//         }else{
+//             reject("something went wrong")
+//         }
+
+//     }, 1000)
+// })
+
+// promiseFour
+// .then( (info)=>{
+//     console.log(info)
+//     return info.username
+// })
+// .then( (username)=>{
+//     console.log(username)
+// })
+// .catch( (error)=>{
+//     console.log(error)
+// })
+// .finally( ()=>{
+//     console.log("the promise is either resolved or rejected")
+// })
+
+const promiseFive=new Promise(function (resolve, reject){
+    setTimeout(function (){
+        let error =true
+        if(!error){
+           resolve({username:"sinan", city:"dubai"}) 
+        }else{
+            reject("oops something went wrong")
+        }
+    },1000)
+})
+
+async function consumePrimiseFive(){
+    try {
+        const response=await promiseFive
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+consumePrimiseFive()
+
+
